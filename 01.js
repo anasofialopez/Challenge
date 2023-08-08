8e5a8e7738ec3a765521fac22a3877ce519b024f
anaslopezl <133251006+anaslopezl@users.noreply.github.com>
2023-08-08 11:04:53 -0500

    01

---
+++ 01.js
@@ -0,0 +1,23 @@
+//Tradicional
+function numero_par (a){
+	if ((a%2)==0){
+	return("Numero Par");
+	} else {
+	return("Numero Impar");
+}
+
+//Regular 
+const numero_par = function (a){
+	if ((a%2)==0){
+	return("Numero Par");
+	} else {
+	return("Numero Impar");
+}
+
+//Flecha
+const numero_par = (a) => {
+	if ((a%2)==0){
+	return("Numero Par");
+	} else {
+	return("Numero Impar");
+}
